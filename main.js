e52a30dad14d25d89b98268cf748e30c5932d445
tonrea333 <readushartoniii@Readuss-MacBook-Pro.local>
2021-07-19 22:24:37 -0400

    all

--- main.js
+++ main.js
@@ -16,20 +16,38 @@
 //const featureOl = document.querySelector("#a");
 
 
-
+//const for orderlist
 const featureOl = document.querySelector("#myOl");
-
+//orderlist onclick event listener
 featureOl.addEventListener("click", onClick);
-
+//function to line through selected text
 function onClick(event){
    event.target.style.textDecoration = "line-through"
 }
-
+//const for unordered list
 const featuretwoOl = document.querySelector("#myUl");
 console.log(featuretwoOl)
+//unordered list receives eventlistner for click
 featuretwoOl.addEventListener("click", onClick2);
-
+//function to change opacity of clicked name
 function onClick2(e){
-    e.target.style.transition  = "all 2s";
+    e.target.style.opacity = ".1";
     console.log()
+}
+
+//const for images on document
+const featureImg = document.querySelector("#row");
+//add event listener for click image
+featureImg.addEventListener("click", onClick3);
+//function to change width of image when clicked
+function onClick3(e){
+    e.target.style.width = "1px";
+}
+const button = document.querySelector("#destroy-all");
+button.addEventListener("click", onClick4);
+
+function onClick4(){
+
+
+
 }
